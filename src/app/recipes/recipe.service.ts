bd80a5be7320e4b98c7fa3a57509ead0bdd93de2
scallopy <scallopy@abv.bg>
2019-05-09 13:20:54 +0300

    Adding ingredients to the recipe-detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,30 @@
 import { EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   @Output() recipeSelected = new EventEmitter<Recipe>();
   
   private recipes: Recipe[] = [
     new Recipe(
-      'Test Recipe', 
-      'This is a simple Recipe', 
-      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg'),
+      'Testy Schnitzel', 
+      'A super-testy Schnitzel - just awesome!', 
+      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
      new Recipe(
-      'New Test Recipe', 
-      'This is a simple Recipe', 
-      'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
+      'Big Fet Burger', 
+      'What else you need to say?', 
+       'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg',
+       [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+       ]
+     )
   ];
 
   getRecipes() {
