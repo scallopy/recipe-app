b2b79a45caf4c35ecf44ac0c649e1f5458f6768f
scallopy <scallopy@abv.bg>
2019-05-16 12:16:58 +0300

    Get Recipe via ResipeService and id

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -23,6 +23,7 @@ export class RecipeDetailComponent implements OnInit {
       .subscribe(
         (params: Params) => {
           this.id = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
         }
       );
   }
