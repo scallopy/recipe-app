e52ce9b1c9a5a70447404a3d4da34c9b8b3c68ca
scallopy <scallopy@abv.bg>
2019-06-19 17:40:24 +0300

    Allowing the Selection of Items in the List

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,24 +1,36 @@
 import { 
   Component, 
-  OnInit, 
+  OnInit,
+  OnDestroy
 } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  editMode = false;
+  editedItemIndex: number;
 
   constructor(
     private shoppingListService: ShoppingListService
   ) { }
 
   ngOnInit() {
+    this.subscription = this.shoppingListService.startEditing
+      .subscribe(
+        (index: number) => {
+          this.editedItemIndex = index;
+          this.editMode = true;
+        }
+      );
   }
 
   onAddItem(form: NgForm) {
@@ -27,4 +39,7 @@ export class ShoppingEditComponent implements OnInit {
     this.shoppingListService.addIngredient(newIngredient);
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
